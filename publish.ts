e3858fdb8d0a7f867b1c8f90d893984ec8eac3a5
X <i@jex.me>
2021-02-05 23:55:19 +0800

    refactor: update scripts

--- publish.ts
+++ publish.ts
@@ -1,9 +1,14 @@
 /** `postpublish` will be invoked before publish. */
 export async function prepublish(version: string, message: string) {
-    console.log('on prepublish', version, message)
+    const readme = await Deno.readTextFile('./README.md')
+
+    await Deno.writeTextFile('./README.md', readme.replace(
+        /\/\/deno\.land\/x\/publish@v[\d\.]+\//,
+        `//deno.land/x/publish@v${version}/`
+    ))
 }
 
 /** `postpublish` will be invoked after publish. */
 export async function postpublish() {
-    console.log('on postpublish')
+    console.log('Done')
 }
