6ea76683d63e95ea1412c5c396d8dde0e9afe421
X <i@jex.me>
2021-02-05 23:47:46 +0800

    doc: update jsdoc

--- publish.ts
+++ publish.ts
@@ -1,9 +1,9 @@
-/* this functon will be invoked before publish */
+/** `postpublish` will be invoked before publish. */
 export async function prepublish(version: string, message: string) {
     console.log('on prepublish', version, message)
 }
 
-/* this functon will be invoked after publish */
+/** `postpublish` will be invoked after publish. */
 export async function postpublish() {
     console.log('on postpublish')
 }
